4cae4a55441e0255f8b97186ee809258170ea447
jalethesh <james.lee.intelligent@gmail.com>
2022-07-11 04:22:18 -0400

    remove share component

--- pages/components/shared/scrollbutton/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function ScrollButton () {
-  return(
-    <section id="section03" className={styles.scrollbutton}>
-      <a href="#section04"><span></span>Scroll</a>
-    </section>
-  )
-}
